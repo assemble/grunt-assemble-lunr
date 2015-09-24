2f2f44c03829648f01013512e88e394ec700a533
Brian Woodward <brian.woodward@gmail.com>
2015-09-24 16:56:17 -0400

    reverting code to grunt-assemble

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var lunr = require('lunr');
 var _ = require('lodash');
 
 
-var idx = lunr(function () {
+var idx = lunr(function() {
   this.ref('url');
   this.field('title', { boost: 10 });
   this.field('tags', { boost: 100 });
@@ -24,7 +24,7 @@ var idx = lunr(function () {
 
 var cache = {};
 
-var addCacheItem = function (page) {
+var addCacheItem = function(page) {
   cache[page.dest] = {
     url: page.dest,
     title: page.title || page.dest,
@@ -33,7 +33,7 @@ var addCacheItem = function (page) {
   };
 };
 
-var updateCacheItem = function (page, content) {
+var updateCacheItem = function(page, content) {
   var $ = cheerio.load(content);
   cache[page.dest].body = $('body').text();
 };
@@ -42,72 +42,66 @@ var options = {
   stage: 'render:*:*'
 };
 
-module.exports = function (assemble) {
 
-  var opts = assemble.config;
+/**
+ * Lunr Search Plugin
+ * @param  {Object}   params
+ * @param  {Function} callback
+ */
+module.exports = function(params, callback) {
+  'use strict';
 
-  /**
-   * Lunr Search Middleware
-   * @param  {Object}   params
-   * @param  {Function} callback
-   */
-  var middleware = function(params, callback) {
-
-    opts.lunr = opts.lunr || {
-      dataPath: path.join(process.cwd(), 'search_index.json')
-    };
-
-    // call before each page is rendered to get
-    // information from the context
-    var gatherSearchInformation = function () {
-      addCacheItem(params.page);
-      params.context.lunr = params.context.lunr || {};
-      params.context.lunr.dataPath = params.context.lunr.dataPath || 'search_index.json';
-    };
-
-    var indexPageContent = function () {
-      updateCacheItem(params.page, params.content);
-    };
-
-    var generateSearchDataFile = function () {
-      _.forOwn(cache, function(item, url) {
-        idx.add(item);
-      });
-
-      fs.writeFileSync(opts.lunr.dataPath, JSON.stringify(idx));
-    };
-
-    switch(params.event) {
-
-      // for each page, gather search data and add path of
-      // index file to context
-      case 'page:before:render':
-        gatherSearchInformation();
-        break;
-
-
-      // get the rendered data for each page and add to search
-      // data index file.
-      case 'page:after:render':
-        indexPageContent();
-        break;
-
-      // after rendering all the pages
-      // build up the final search data index file
-      case 'assemble:after:render':
-        generateSearchDataFile();
-        break;
-
-      default:
-        break;
-    }
-
-    callback();
+  var opts = params.assemble.options;
+  opts.lunr = opts.lunr || {
+    dataPath: path.join(process.cwd(), 'search_index.json')
   };
 
-  middleware.events = [];
-  return {
-    'assemble-middleware-lunr': middleware
+  // call before each page is rendered to get
+  // information from the context
+  var gatherSearchInformation = function () {
+    addCacheItem(params.context.page);
+    params.context.lunr = params.context.lunr || {};
+    params.context.lunr.dataPath = params.context.lunr.dataPath || 'search_index.json';
   };
 
+  var indexPageContent = function () {
+    updateCacheItem(params.page, params.content);
+  };
+
+  var generateSearchDataFile = function () {
+    _.forOwn(cache, function(item, url) {
+      idx.add(item);
+    });
+
+    fs.writeFileSync(opts.lunr.dataPath, JSON.stringify(idx));
+  };
+
+  switch(params.stage) {
+
+    // for each page, gather search data and add path of
+    // index file to context
+    case 'render:pre:page':
+      gatherSearchInformation();
+      break;
+
+
+    // get the rendered data for each page and add to search
+    // data index file.
+    case 'render:post:page':
+      indexPageContent();
+      break;
+
+    // after rendering all the pages
+    // build up the final search data index file
+    case 'render:post:pages':
+      generateSearchDataFile();
+      break;
+
+    default:
+      break;
+  }
+
+  callback();
 };
+
+module.exports.options = options;
